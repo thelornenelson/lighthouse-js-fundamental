4964e1a61f644f7d859e4cbd668d3a09bd7c33ec
Andrew Barclay <andrewkbarclay@gmail.com>
2017-11-24 21:18:24 +0000

    adding whichSchool.js

---
+++ whichSchool.js
@@ -0,0 +1,11 @@
+function whichSchool(age){
+  if(age < 13)
+    return "Elementary School";
+  else if(age >= 13 && age <= 18)
+         return "Secondary School";
+       else return "Lighthouse Labs";
+}
+
+console.log(whichSchool(35));
+console.log(whichSchool(8));
+console.log(whichSchool(14));
